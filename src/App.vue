88947a5ec486cfaaf8249adacac253b0150a6c91
JobbyM <songzhipengmune@gmail.com>
2016-09-07 18:12:55 +0800

    init vue-radon-ui

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class=container>
+      <rd-datepicker :date="datePicker"></rd-datepicker>
+    </div>
+  </div>
+</template>
+
+<script>
+import { rdDatepicker } from 'radon-ui'
+
+export default {
+  data () {
+    return {
+      datePicker: {
+        value: '',
+        options: {
+          quickClose: true,
+          format: 'YYYY/MM/DD'
+        }
+      }
+    }
+  },
+  components: {
+    rdDatepicker
+  }
+}
+</script>
+
+<style src=radon-ui/dist/static/css/dist.css></style>
+<style>
+  .container {
+    width: 200px;
+    height: 30px;
+  }
+</style>
